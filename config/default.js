89c9b527ad7d5daec449fe28797c2349d8153142
Vitalii Derda <derdavitaliy@gmail.com>
2024-01-28 15:03:08 +0200

    feat: added homework 2

---
+++ config/default.js
@@ -0,0 +1,4 @@
+module.exports = {
+  logLevel: process.env.LOG_LEVEL,
+  colorsEnabled: process.env.COLORS_ENABLED,
+};
--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+// Packages
+require('dotenv').config();
+// Utils
 const logger = require('./utils/logger')('main');
 
-logger.info('the script is running!');
+logger.info('info log - the script is running!');
+logger.warn('warn log - the script is running!');
+logger.error('error log - the script is running!');
