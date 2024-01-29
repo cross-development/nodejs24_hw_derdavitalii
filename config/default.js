e8abe7c66a552c0e1ee95fbcf3be68f3a3987042
Vitalii Derda <derdavitaliy@gmail.com>
2024-01-29 08:38:48 +0200

    fix: added default values to the config

--- config/default.js
+++ config/default.js
@@ -1,4 +1,4 @@
 module.exports = {
-  logLevel: process.env.LOG_LEVEL,
-  colorsEnabled: process.env.COLORS_ENABLED,
+  logLevel: process.env.LOG_LEVEL ?? 'warn',
+  colorsEnabled: process.env.COLORS_ENABLED ?? 0,
 };
